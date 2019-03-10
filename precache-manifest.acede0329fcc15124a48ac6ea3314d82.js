22e25fa223c0ae0a66562a093a7c48db842df961
Jose Luis Poveda <jlpoveda@stackscale.com>
2019-03-10 21:16:17 +0100

    first commit

---
+++ precache-manifest.acede0329fcc15124a48ac6ea3314d82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cuentacuentos/robots.txt"
+  },
+  {
+    "revision": "dafed3cc861fcf2e29ed",
+    "url": "/cuentacuentos/js/chunk-vendors.0e0eca66.js"
+  },
+  {
+    "revision": "689b9626cd089eb0ed25",
+    "url": "/cuentacuentos/js/chunk-588cf876.8b5d5d2f.js"
+  },
+  {
+    "revision": "1bd63114cb000d8f0a6f",
+    "url": "/cuentacuentos/js/app.f3bfa674.js"
+  },
+  {
+    "revision": "7d705f9a1a77336ed921",
+    "url": "/cuentacuentos/js/about.2b6705d6.js"
+  },
+  {
+    "revision": "f5accda37cbd127f2b1f251e003fa481",
+    "url": "/cuentacuentos/index.html"
+  },
+  {
+    "revision": "689b9626cd089eb0ed25",
+    "url": "/cuentacuentos/css/chunk-588cf876.d8d099c4.css"
+  },
+  {
+    "revision": "1bd63114cb000d8f0a6f",
+    "url": "/cuentacuentos/css/app.ac906db3.css"
+  }
+];
